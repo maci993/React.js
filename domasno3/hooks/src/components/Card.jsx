6b54eadff6e3407cfb8ad16cc59f5c53049dd99a
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2024-03-31 22:29:38 +0200

    Add files via upload

---
+++ domasno3/hooks/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = ({ title }) => {
+    return (
+        <div>
+           <h1>Welcome!</h1>
+            <p>Successfully created your account!</p>
+        </div>
+    )
+};
+
+export default Card;
+
